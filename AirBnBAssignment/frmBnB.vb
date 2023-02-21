cf4f046517b80e1f9b4622c8b74004e0acb6d5ad
Promethibot <paola.mancini@me.com>
2023-02-21 15:18:01 -0500

    Made more in line with requirements

--- AirBnBAssignment/frmBnB.vb
+++ AirBnBAssignment/frmBnB.vb
@@ -1,17 +1,18 @@
 ﻿Public Class frmBnB
 
-    Const _cdecNightlyCost As Decimal = 79.0
-    Dim nightsTotal As Decimal = 0.00
+    Const _cdecCostPerNight As Decimal = 79.0
+    Dim intNumNights As Integer = 0
+    Dim strNumNights As String
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Application.Exit()
     End Sub
 
     Private Sub btnCost_Click(sender As Object, e As EventArgs) Handles btnCost.Click
-        If Decimal.TryParse(tbxUsIn.Text, nightsTotal) Then
+        If Decimal.TryParse(strNumNights, intNumNights) Then
             lblError.Visible = False
-            lblPrice.Text = "$" + (_cdecNightlyCost * nightsTotal).ToString("N2")
-            btnClear.Enabled = true
+            lblPrice.Text = "$" + (_cdecCostPerNight * intNumNights).ToString("N2")
+            btnClear.Enabled = True
         Else
             lblError.Visible = True
         End If
@@ -22,15 +23,21 @@
         lblPrice.Text = ""
         tbxUsIn.Text = ""
         btnClear.Enabled = False
+        tbxUsIn.Select()
     End Sub
 
     Private Sub tbxUsIn_TextChanged(sender As Object, e As EventArgs) Handles tbxUsIn.TextChanged
         If tbxUsIn.TextLength > 0 Then
             btnCost.Enabled = True
             btnClear.Enabled = True
+            strNumNights = tbxUsIn.Text
         End If
         If tbxUsIn.TextLength = 0 Then
             btnCost.Enabled = False
         End If
+    End Sub
+
+    Private Sub frmBnB_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tbxUsIn.Select()
     End Sub
 End Class
